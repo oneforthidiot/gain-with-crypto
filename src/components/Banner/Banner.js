6cab0f5de0fa6582e80a39a02d4888e07291ff06
Harshit Pandey <harshitpandey64@gmail.com>
2023-01-17 05:29:48 +0000

    SEO Changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Stay up-to-date on the latest cryptocurrency prices with our real-time crypto price tracker. Track Bitcoin, Ethereum, Litecoin, and other top digital assets. Get alerts, charts, and analysis on the crypto market. 
+            Stay up-to-date on the latest cryptocurrency prices with our real-time crypto price tracker. Track Bitcoin, Ethereum, Litecoin, and other top digital assets. 
           </Typography>
         </div>
         <Carousel />
